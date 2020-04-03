703b040459092bdff1a7c37904074c8476cbc923
rutw <cage0702@gmail.com>
2020-04-03 22:58:07 +0800

    2020-04-03 +jy ver
    add geojson files

--- js/toggle_switch.js
+++ js/toggle_switch.js
@@ -2,15 +2,22 @@ $(function() {
   $('.layerSwitch').change(function() {
     //$('#console-event').html('Toggle: ' + $(this).prop('checked'));
     if ($(this).prop("checked")){ //從關閉變成打開
-      alert("The "+ this.id + " Layer is Open.");
+      //alert("The "+ this.id + " Layer is Open.");
       //alert(this.id + " " +$(this).prop('checked'));
       // 打開此圖層的js
+      let id=this.id;
+      $.getJSON($(this).attr("id")+".geojson",function(data){
+        geojsonlayer=L.geoJson(data);  
+        geojsonlayer.addTo(mymap);
+        myLayer.set(id,geojsonlayer);
+      });
+
     }
     else{  //從打開變成關閉 
-      alert("The "+ this.id + " Layer is Close.");
-      // 關閉此圖層的js
+      //alert("The "+ this.id + " Layer is Close.");
+      myLayer.get(this.id).remove();
+      myLayer.set(this.id,null);
     }
-    
   })
 })
 
